251d54a0e9cc4499a4e8fbd4dc71dd46dafbf78b
kristynerhaugen <kristynerhaugen@gmail.com>
2022-08-31 12:49:30 -0500

    webpack working with plugins, trying to deploy to heroku

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,9 +1,20 @@
+//import modules
 import "./form";
 import "./submit";
+
+// import css
+import "../css/index.css";
+
+//import Bootstrap
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import images
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
+// add images on load
 window.addEventListener("load", function () {
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+// import html-webpack-plugin
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // add mode here? and set to development?
@@ -8,11 +10,31 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "Webpack Plugin",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
       },
     ],
   },
