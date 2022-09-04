0c450f98de3fb1862a0ad4a217c2e72d3ab368b0
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-04 12:35:25 -0500

    manifest.json created

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,11 @@
 // import html-webpack-plugin
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 //const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
+// const WorkboxPlugin = require("workbox-webpack-plugin");
 // import InjectManifest
 const { InjectManifest } = require("workbox-webpack-plugin");
-
+// require WebpackPwaManifest
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 module.exports = {
@@ -20,6 +21,28 @@ module.exports = {
       template: "./index.html",
       title: "Webpack Plugin",
     }),
+    new WebpackPwaManifest({
+      name: "Contact Cards Application",
+      short_name: "Contact Cards",
+      description: "Keep track of contacts!",
+      background_color: "#7eb4e2",
+      theme_color: "#7eb4e2",
+      start_url: "./",
+      publicPath: "./",
+      icons: [
+        {
+          src: path.resolve("src/images/icon-manifest.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+        {
+          src: path.resolve("src/images/icon-manifest.png"),
+          size: "1024x1024",
+          destination: path.join("assets", "icons"),
+          purpose: "maskable",
+        },
+      ],
+    }),
     new InjectManifest({
       swSrc: "./src/sw.js",
       swDest: "service-worker.js",
@@ -28,12 +51,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
       {
         test: /\.m?js$/,
